22fe75ab2341ef1332d417100ac76077b2f1d4db
pablo147sousa <pablo147sousa@hotmail.com>
2019-10-28 14:45:16 -0300

    Primeiro Commit

---
+++ telas/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render,HttpResponse
+
+# Create your views here.
+def paginaInicial(request):
+    return render(request,"telas/home.html")
+def contato(request):
+    return render(request,"telas/contato.html")     
